93eba441fcbbb51e0fdd7e606d915b80801391e7
ebrillhart <emma.brillhart@gmail.com>
2015-11-03 16:49:02 -0800

    further styling, about, and how-to added

--- static/script.js
+++ static/script.js
@@ -3,4 +3,6 @@ $(document).ready(function(){
     $('.parallax').parallax();
     // materialize side nav collapse function
     $(".button-collapse").sideNav();
+    // materialize open privacy policy modal
+    $('.modal-trigger').leanModal();
 });
